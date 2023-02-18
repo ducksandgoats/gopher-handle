d0d1be415abcfdcffa1c779e0bc3fc0559857614
ducksandgoats <mrtraderjoseph@gmail.com>
2023-02-18 02:16:29 -0600

    update the package

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 module.exports = async function makeGopherFetch(opts = {}) {
   const { makeRoutedFetch } = await import('make-fetch')
-  const {fetch, router} = makeRoutedFetch()
+  const {fetch, router} = makeRoutedFetch({onNotFound: handleEmpty, onError: handleError})
     const Gopher = require('gopher-lib')
     const DEFAULT_OPTS = {}
     const finalOpts = { ...DEFAULT_OPTS, ...opts }
-    // const SUPPORTED_METHODS = ['HEAD', 'GET', 'POST', 'DELETE']
     const gopher = new Gopher.Client(finalOpts)
+
+  function handleEmpty(request) {
+    const { url, headers: reqHeaders, method, body, signal } = request
+    if(signal){
+      signal.removeEventListener('abort', takeCareOfIt)
+    }
+    
+    return {status: 400, headers: {}, body: 'did not find any data'}
+  }
+
+  function handleError(e, request) {
+    const { url, headers: reqHeaders, method, body, signal } = request
+    if(signal){
+      signal.removeEventListener('abort', takeCareOfIt)
+    }
+
+    return {status: 500, headers: {}, body: e.stack}
+  }
 
     function takeCareOfIt(data){
       console.log(data)
@@ -18,28 +35,20 @@ module.exports = async function makeGopherFetch(opts = {}) {
       }
       return theData
     }
-  
-  async function handleGopher(request) {
+
+  router.get('gopher://*/**', async function (request) {
       const { url, method, headers: reqHeaders, body, signal } = request
       if(signal){
         signal.addEventListener('abort', takeCareOfIt)
     }
           const gopherReq = new URL(url)
-    
-          if (gopherReq.protocol !== 'gopher:' || !method || method !== 'GET') {
-            return sendTheData(signal, { status: 400, headers: { 'Content-Type': 'application/json; charset=utf-8' }, body: [Buffer.from('query is incorrect')] })
-          }
 
           if(gopherReq.hostname === '_'){
             return sendTheData(signal, { status: 200, headers: { 'Content-Type': 'text/plain; charset=utf-8' }, body: ['works'] })
           }
-          
-          if(!gopherReq.hostname.startsWith('gopher.')){
-            gopherReq.hostname = 'gopher.' + gopherReq.hostname
-          }
 
           const mainData = await new Promise((resolve, reject) => {
-            gopher.get(gopherReq.toString(), (err, reply)=>{
+            gopher.get(gopherReq.href, (err, reply)=>{
                 if(err) {
                     reject(err);
                 } else {
@@ -48,10 +57,8 @@ module.exports = async function makeGopherFetch(opts = {}) {
               });
             })
             
-            return sendTheData(signal, {status: 200, headers: {'Content-Type': 'text/plain'}, body: [mainData.text]})
-  }
-
-  router.any('gopher://*/**', handleGopher)
+            return sendTheData(signal, {status: 200, headers: {'Content-Type': 'text/plain'}, body: mainData.text})
+  })
 
     return fetch
 }
